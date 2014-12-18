67cd9aecab7a368a4a371c7aea3f6c567b3b9721
Xuezhong Wang <xzwang2005@gmail.com>
2014-12-18 12:26:25 -0500

    first running app

---
+++ Tutorials/Tutorial3_initialize/baseApp.h
@@ -0,0 +1,35 @@
+#include "Application.h"
+
+#include "Win32RenderWindow.h"
+#include "RendererDX11.h"
+
+using namespace Glyph3;
+
+class App : public Application
+{
+
+public:
+	App();
+
+public:
+	virtual void Initialize();
+	virtual void Update();
+	virtual void Shutdown();
+
+	virtual bool ConfigureEngineComponents();
+	virtual void ShutdownEngineComponents();
+
+	virtual void TakeScreenShot();
+
+	virtual bool HandleEvent(EventPtr pEvent);
+	virtual std::wstring GetName();
+
+protected:
+
+	RendererDX11*			m_pRenderer11;
+	Win32RenderWindow*		m_pWindow;
+
+	int						m_iSwapChain;
+	ResourcePtr				m_RenderTarget;
+	ResourcePtr				m_DepthTarget;
+};
